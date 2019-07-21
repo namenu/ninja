9ef779e9049a3173cab05a6687a096579a363855
Hongbo <bob.hongbo.zhang@foxmail.com>
2019-07-21 11:00:57 +0800

    update scripts

--- snapshot.js
+++ snapshot.js
@@ -1,16 +1,15 @@
-
-
-var child = require('child_process')
-var os = require('os')
-var ext = os.platform()
-
-if(ext === 'win32')
-{
+var child = require("child_process");
+var os = require("os");
+var ext = os.platform();
+if (process.argv.includes("-build")) {
+  if (ext === "win32") {
     // running on visual studio command line
-    child.execSync("c:\\Python27\\python.exe configure.py --bootstrap")
-
-} else {
-    child.execSync(`./configure.py --bootstrap`, {stdio:[0,1,2]})
-    child.execSync(`strip ninja`, {stdio:[0,1,2]})        
+    child.execSync("c:\\Python27\\python.exe configure.py --bootstrap");
+  } else {
+    child.execSync(`./configure.py --bootstrap`, { stdio: [0, 1, 2] });
+    child.execSync(`strip ninja`, { stdio: [0, 1, 2] });
+  }
 }
-// git archive --format=tar.gz HEAD -o ./ninja.tar.gz
+if (process.argv.includes("-tar")) {
+  child.exec(`git archive --format=tar.gz HEAD -o ./ninja.tar.gz`);
+}
