a25eaf48154a71619911888f545c7be1538b2d2d
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2020-11-27 10:43:40 +0800

    tweak scripts

--- snapshot.js
+++ snapshot.js
@@ -1,20 +1,25 @@
 var child = require("child_process");
+var fs = require("fs");
 var os = require("os");
+const path = require("path");
 var ext = os.platform();
 var command = "python configure.py --bootstrap --verbose";
 function build() {
   if (ext === "win32") {
     // running on visual studio command line
-    child.execSync(command,{cwd:__dirname});
+    child.execSync(command, { cwd: __dirname });
   } else {
     if (process.platform === "darwin") {
       process.env["CXXFLAGS"] = "-flto";
     }
-    child.execSync(command, { stdio: [0, 1, 2], cwd : __dirname });
-    child.execSync(`strip ninja`, { stdio: [0, 1, 2], cwd : __dirname });
+    child.execSync(command, { stdio: [0, 1, 2], cwd: __dirname });
+    child.execSync(`strip ninja`, { stdio: [0, 1, 2], cwd: __dirname });
   }
 }
-exports.build = build
-if(require.main === module ){
-  build()
+exports.build = build;
+if (require.main === module) {
+  build();
+  var src = path.join(__dirname, `ninja${ext === "win32" ? ".exe" : ""}`);
+  var dst = path.join(__dirname, "..", ext, `ninja.exe`);
+  fs.copyFileSync(src, dst);
 }
